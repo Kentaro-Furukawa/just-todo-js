48695f7831f8f7ad0ba7d0b6878a0ed4c8388fb5
Kentaro Furukawa <info@ken89.com>
2023-08-04 23:08:26 +0900

    add complete button and delete button. use object instead of array to store todo items.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const todoItems = [];
+let todoItems = [];
+
+class ToDoItem {
+  constructor(id, task, isCmpleted) {
+    this.id = id;
+    this.task = task;
+    this.isCmpleted = isCmpleted;
+  }
+
+}
+
+function uuidv4() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
 
 function renderTodoList() {
   const todoList = document.getElementById('todoList')
@@ -14,7 +29,31 @@ function renderTodoList() {
   todoItems.forEach(todoItem => {
 
     const todoListItemElement = document.createElement('li')
-    todoListItemElement.innerHTML = `<span>${todoItem}</span>`;
+
+    let innerElement = ''
+
+    if (todoItem.isCmpleted) {
+      innerElement = document.createElement('s')
+    } else {
+      innerElement = document.createElement('span')
+    }
+
+    innerElement.innerText = todoItem.task;
+    todoListItemElement.append(innerElement)
+
+    const completeButton = document.createElement('button')
+    completeButton.setAttribute("type", "submit")
+    completeButton.setAttribute("value", todoItem.id)
+    completeButton.setAttribute("onClick", `completeTodoItem("${todoItem.id}")`)
+    completeButton.innerText = "Complete"
+    todoListItemElement.append(completeButton)
+
+    const deleteButton = document.createElement('button')
+    deleteButton.setAttribute("type", "submit")
+    deleteButton.setAttribute("value", todoItem.id)
+    deleteButton.setAttribute("onClick", `deleteTodoItem("${todoItem.id}")`)
+    deleteButton.innerText = "Delete"
+    todoListItemElement.append(deleteButton)
 
     todoListElement.append(todoListItemElement)
 
@@ -23,17 +62,41 @@ function renderTodoList() {
   content = todoListElement
   todoList.appendChild(content)
 }
+
 renderTodoList();
 
-const addButton = document.getElementById('addBtn')
+
+const addButton = document.getElementById('addButton')
+
 addButton.addEventListener('click', () => {
   const input = document.getElementById('todoInput')
   inputValue = input.value.trim()
   if (!inputValue) {
     return
   }
-  todoItems.push(inputValue);
+  const newTodoItem = new ToDoItem(uuidv4(), inputValue, false)
+  todoItems.push(newTodoItem);
 
   renderTodoList();
+
   input.value = '';
 })
+
+function completeTodoItem(targetID) {
+  todoItems.forEach(todoItem => {
+    if (todoItem.id == targetID) {
+      todoItem.isCmpleted = !todoItem.isCmpleted
+    }
+  });
+
+  renderTodoList();
+}
+
+function deleteTodoItem(targetID) {
+  console.log("deletes", targetID)
+  todoItems = todoItems.filter(
+    (todoItem) => todoItem.id != targetID
+  )
+
+  renderTodoList();
+}
